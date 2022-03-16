4c9cc98db5e509df3c245a1ef504472a2484adfc
pravinsdabhade <pravinsdabhade44@yahoo.in>
2022-03-16 20:26:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hbakxo-stbkgf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hbakxo-stbkgf)
---
+++ app/table-filtering-example.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { FormControl } from '@angular/forms';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
+
+/**
+ * @title Table with filtering
+ */
+@Component({
+  selector: 'table-filtering-example',
+  styleUrls: ['table-filtering-example.css'],
+  templateUrl: 'table-filtering-example.html',
+})
+export class TableFilteringExample implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  positionFilter = new FormControl();
+  nameFilter = new FormControl();
+  globalFilter = '';
+
+  filteredValues = {
+    position: '', name: '', weight: '',
+    symbol: ''
+  };
+  ngOnInit() {
+
+    this.positionFilter.valueChanges.subscribe((positionFilterValue) => {
+      this.filteredValues['position'] = positionFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
+
+    this.nameFilter.valueChanges.subscribe((nameFilterValue) => {
+      this.filteredValues['name'] = nameFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
+
+    this.dataSource.filterPredicate = this.customFilterPredicate();
+
+  }
+
+  applyFilter(filter) {
+    this.globalFilter = filter;
+    this.dataSource.filter = JSON.stringify(this.filteredValues);
+  }
+
+  // numFilter(filterValue: string) {
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  //   this.dataSource.filterPredicate = (data: any, fitlerString: string) => {
+
+  //       return data.position == filterValue;
+  //   };
+  //   this.dataSource.filter = filterValue;
+  // }
+
+  customFilterPredicate() {
+    const myFilterPredicate = (data: PeriodicElement, filter: string): boolean => {
+      var globalMatch = !this.globalFilter;
+
+      if (this.globalFilter) {
+        // search all text fields
+        globalMatch = data.name.toString().trim().toLowerCase().indexOf(this.globalFilter.toLowerCase()) !== -1;
+      }
+
+      if (!globalMatch) {
+        return;
+      }
+
+      let searchString = JSON.parse(filter);
+      return data.position.toString().trim().indexOf(searchString.position) !== -1 &&
+        data.name.toString().trim().toLowerCase().indexOf(searchString.name.toLowerCase()) !== -1;
+    }
+    return myFilterPredicate;
+  }
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
